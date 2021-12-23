1851cce819fb4f3cd2f97b81a1e9b76bb65c9881
tubi55 <tubi55@nate.com>
2021-12-23 11:32:42 +0900

    라우터 기록

--- src/main/Main.js
+++ src/main/Main.js
@@ -2,7 +2,7 @@ function Main(){
   return (
     <section className="content main">
       <div className="inner">
-        <h1>Main Page</h1>
+        <h1>Main Page2</h1>
       </div>
     </section>
   )
