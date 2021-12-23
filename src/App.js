155d6f41136ffec2b8c0859e8844bda906cd3c9b
tubi55 <tubi55@nate.com>
2021-12-23 10:47:11 +0900

    서브 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import "./css/style.css";
 import Header from "./common/Header.js";
 import Footer from "./common/Footer.js";
 import Main from "./main/Main.js";
+import Department from "./sub/Department.js";
+import Board from "./sub/Board.js";
+import Gallery from "./sub/Gallerly.js";
+import Location from "./sub/Location.js";
+import Membership from "./sub/Membership.js";
+import Youtube from "./sub/Youtube.js";
 
 function App() {
   return (
     <div className="App"> 
       <Header />
-
       <Main />
-
       <Footer />
     </div>
   );
---
+++ src/sub/Board.js
@@ -0,0 +1,11 @@
+function Board(){
+  return (
+    <section className="content board">
+      <div className="inner">
+        <h1>Board</h1>
+      </div>
+    </section>
+  )
+}
+
+export default Board;
