b5565b8daaf8a9a371c7edf05bc21d14c426fdd0
tubi55 <tubi55@nate.com>
2021-12-24 14:25:26 +0900

    useRef로 dom 선택 후 클래스명 주기

--- src/sub/Gallery.js
+++ src/sub/Gallery.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function Gallery(){
   const baseURL = "https://www.flickr.com/services/rest/?";
@@ -10,23 +10,42 @@ function Gallery(){
 
   //flickr데이터 배열을 받을 빈 배열을 items이름의 state로 설정
   let [items, setItems] = useState([]);
+  let list = useRef(null);
+  
 
   //useEffect의 의존성을 비워서 처음 로딩시 한번만 실행되게 설정
   useEffect(()=>{
+    
+    callData();   
+
+  },[]);
+
+  async function callData(){
+    await getFlickr();
+    delay();
+  }
+
+  function getFlickr(){
     axios
     .get(url)
     .then(json=>{
       console.log(json.data.photos.photo);
       setItems(json.data.photos.photo);
     })
-  },[]);
+  }
+
+  function delay(){
+    setTimeout(()=>{
+      list.current.classList.add("on");
+    },5000);
+  }
 
   return (
     <section className="content gallery">
       <div className="inner">
         <h1>Gallery</h1>
 
-        <ul className="list">
+        <ul className="list" ref={list}>
           {
             items.map((item, index)=>{
               const imgSrc = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`;
