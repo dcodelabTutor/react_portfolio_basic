92777facb40af20cd3144371afbeaae38ccd5128
tubi55 <tubi55@nate.com>
2022-01-03 14:54:36 +0900

    라우터 최종 수정

--- src/common/Header.js
+++ src/common/Header.js
@@ -6,16 +6,16 @@ function Header(){
     <header>
       <div className="inner">
         <h1>
-        <NavLink activeStyle={active} exact to="/">DCODELAB-router</NavLink>
+        <NavLink activeStyle={active} exact to="/react_portfolio_basic">DCODELAB-router</NavLink>
         </h1>
 
         <ul id="gnb">
-          <li><NavLink activeStyle={active} exact to="/department">DEPARTMENT2</NavLink></li>
-          <li><NavLink activeStyle={active} exact to="/board">BOARD</NavLink></li>
-          <li><NavLink activeStyle={active} exact to="/gallery">GALLERY</NavLink></li>
-          <li><NavLink activeStyle={active} exact to="/youtube">YOUTUBE</NavLink></li>
-          <li><NavLink activeStyle={active} exact to="/location">LOCATION</NavLink></li>
-          <li><NavLink activeStyle={active} exact to="/membership">MEMBERSHIP</NavLink></li>
+          <li><NavLink activeStyle={active} exact to="/react_portfolio_basic/department">DEPARTMENT2</NavLink></li>
+          <li><NavLink activeStyle={active} exact to="/react_portfolio_basic/board">BOARD</NavLink></li>
+          <li><NavLink activeStyle={active} exact to="/react_portfolio_basic/gallery">GALLERY</NavLink></li>
+          <li><NavLink activeStyle={active} exact to="/react_portfolio_basic/youtube">YOUTUBE</NavLink></li>
+          <li><NavLink activeStyle={active} exact to="/react_portfolio_basic/location">LOCATION</NavLink></li>
+          <li><NavLink activeStyle={active} exact to="/react_portfolio_basic/membership">MEMBERSHIP</NavLink></li>
         </ul>
       </div>
     </header>
