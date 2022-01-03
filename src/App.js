f0f3c85f1e4e564fd289c785d9551df905ecb707
tubi55 <tubi55@nate.com>
2022-01-03 15:17:01 +0900

    Test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {HashRouter, Switch, Route} from "react-router-dom";
+import { Switch, Route} from "react-router-dom";
 import "./css/style.css";
 import Header from "./common/Header.js";
 import Footer from "./common/Footer.js";
@@ -13,18 +13,16 @@ import Youtube from "./sub/Youtube.js";
 function App() {
   return (
     <div className="App"> 
-      <Header />
-          <HashRouter>
-            <Switch>
-              <Route  path="/" component={Main}></Route> 
+      <Header /> 
+            {/* <Switch> */}
+              <Route  exact path="/" component={Main}></Route> 
               <Route  path="/department" component={Department}></Route>
               <Route  path="/board" component={Board}></Route>
               <Route  path="/gallery" component={Gallery}></Route>
               <Route  path="/youtube" component={Youtube}></Route>
               <Route  path="/location" component={Location}></Route>
               <Route  path="/membership" component={Membership}></Route>  
-            </Switch>
-          </HashRouter>
+            {/* </Switch>    */}
       <Footer />      
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-//import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
      
   </React.StrictMode>,
   
