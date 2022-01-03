af202252172abde91dfe2171c04786f7c1202689
tubi55 <tubi55@nate.com>
2022-01-03 14:30:53 +0900

    router 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {HashRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import "./css/style.css";
 import Header from "./common/Header.js";
 import Footer from "./common/Footer.js";
@@ -9,21 +9,24 @@ import Gallery from "./sub/Gallery.js";
 import Location from "./sub/Location.js";
 import Membership from "./sub/Membership.js";
 import Youtube from "./sub/Youtube.js";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
     <div className="App"> 
       <Header />
 
-      <HashRouter>
-        <Route exact path="/" component={Main}></Route> 
-        <Route exact path="/department" component={Department}></Route>
-        <Route exact path="/board" component={Board}></Route>
-        <Route exact path="/gallery" component={Gallery}></Route>
-        <Route exact path="/youtube" component={Youtube}></Route>
-        <Route exact path="/location" component={Location}></Route>
-        <Route exact path="/membership" component={Membership}></Route>  
-      </HashRouter>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/a" component={Main}></Route> 
+          <Route exact path="/a/department" component={Department}></Route>
+          <Route exact path="/a/board" component={Board}></Route>
+          <Route exact path="/a/gallery" component={Gallery}></Route>
+          <Route exact path="/a/youtube" component={Youtube}></Route>
+          <Route exact path="/a/location" component={Location}></Route>
+          <Route exact path="/a/membership" component={Membership}></Route>  
+        </Switch>
+      </BrowserRouter>
           
       <Footer />      
     </div>
