d4883d41839a773cd0f38c6ffe8a54f612d820f3
tubi55 <tubi55@nate.com>
2022-01-03 15:20:32 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     
-      <HashRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
-      </HashRouter>
+      </BrowserRouter>
      
   </React.StrictMode>,
   
