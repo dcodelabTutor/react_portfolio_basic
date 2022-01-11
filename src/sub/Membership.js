1b8fe5fc76be2b97f49b6cdfa4136e61a50ce406
tubi55 <tubi55@nate.com>
2022-01-11 20:56:41 +0900

    폼 동적 생성

--- src/sub/Membership.js
+++ src/sub/Membership.js
@@ -1,8 +1,85 @@
-function Membership(){
+import { useRef, useState } from 'react';
+import FormInput from './FormInput';
+
+function Membership(){  
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    birthday: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: 'username',
+      type: 'text',
+      placeholder: 'Username',
+      label: 'Username'
+    },
+    {
+      id: 2,
+      name: 'email',
+      type: 'text',
+      placeholder: 'Email',
+      label: 'Email'
+    },
+    {
+      id: 3,
+      name: 'birthday',
+      type: 'text',
+      placeholder: 'Birthday',
+      label: 'Birthday'
+    },
+    {
+      id: 4,
+      name: 'password',
+      type: 'text',
+      placeholder: 'Password',
+      label: 'Password'
+    },
+    {
+      id: 5,
+      name: 'confirmPassword',
+      type: 'text',
+      placeholder: 'Confirm Password',
+      label: 'Confirm Password'
+    }
+  ];
+
+
+  function handleSubmit(e){
+    e.preventDefault();
+  }
+
+  function onChange(e){
+    setValues({...values, [e.target.name]:e.target.value})
+  }
+
+  console.log(values);
+
   return (
     <section className="content membership">
       <div className="inner">
         <h1>Membership</h1>
+
+        <form onSubmit={handleSubmit}>    
+          {
+            inputs.map((input)=>{
+              return (
+                <FormInput 
+                  key={input.id} 
+                  {...input} 
+                  value={values[input.name]} 
+                  onChange={onChange} 
+                />   
+              )
+              
+            })
+          }              
+          <button >Submit</button>
+        </form>
       </div>
     </section>
   )
